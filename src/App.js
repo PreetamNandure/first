8e77703664962438b196ff5571aa44a7be128fb3
Preetam Nandure <preetam.nandure@gmail.com>
2021-07-29 00:18:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+
+import React from 'react'
+import { Link, BrowserRouter as Router ,Route} from 'react-router-dom'
+import ProductCreate from './Ecom/ProductCreate'
+import ProductList from './Ecom/ProductList'
+import Dashboard from './Ecom/Dashboard'
+import { AppBar, Button } from '@material-ui/core'
+
+function App(){
+
+return(
+
+    <div>
+    <Router>
+    
+    <menu>
+    <Link to="/"><Button color="secondary" variant="contained">Dashboard</Button></Link>
+    <Link to="/create"><Button color="secondary" variant="contained">Create Product</Button></Link>
+    <Link to="/list"><Button color="secondary" variant="contained">Product List</Button></Link>
+    <Link to="/list"><Button color="secondary" variant="contained">Product Menu</Button></Link>
+    </menu>
+    
+    <Route path="/create"><ProductCreate/></Route>
+    <Route path="/list"><ProductList/></Route>
+    <Route exact path="/"><Dashboard/></Route>
+     
+    
+    </Router>   
+    
+    </div>
+
+
+)
+
+
+}
+export default App;
